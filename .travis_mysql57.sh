e0f87b181c530e91235b4cc80ce261b27f5b6587
Takeshi SHINODA <takeshinoda@gmail.com>
2016-12-21 01:41:58 +0900

    testtest

--- .travis_mysql57.sh
+++ .travis_mysql57.sh
@@ -9,4 +9,5 @@ apt-get update -qq
 apt-get install -qq mysql-server libmysqlclient-dev
 
 # https://www.percona.com/blog/2016/03/16/change-user-password-in-mysql-5-7-with-plugin-auth_socket/
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''"
+#mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '';
