5811933e4629cc0693850b2dc6774be1f9f2e81a
Takeshi SHINODA <takeshinoda@gmail.com>
2016-12-21 02:16:07 +0900

    fffffffiiiixxxssssss

--- .travis_mysql57.sh
+++ .travis_mysql57.sh
@@ -8,8 +8,6 @@ add-apt-repository 'deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7'
 apt-get update -qq
 apt-get install -qq mysql-server libmysqlclient-dev
 
-cat /var/log/mysqld.log | grep password
-
 # https://www.percona.com/blog/2016/03/16/change-user-password-in-mysql-5-7-with-plugin-auth_socket/
 #mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''"
 mysql -u root -e "UPDATE mysql.user SET authentication_string = PASSWORD(‘’), plugin = ‘mysql_native_password’ WHERE User = ‘root’ AND Host = ‘localhost’;"
